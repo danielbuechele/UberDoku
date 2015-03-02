abbf4e25ebff791321c2289a12ec592fc6595871
Daniel Büchele <daniel@buechele.cc>
2015-03-02 23:13:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# UberDoku
+by Daniel Büchele
+
+This is an implementation of a web-based Soduku game.
+
+## Setup
+```
+npm install
+webpack
+```
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	entry: './js/app.jsx',
+	module: {
+		loaders: [
+			{ test: /\.jsx$/,    loader: 'jsx-loader'},
+			{ test: /\.less$/,   loader: "style-loader!css-loader!less-loader" },
+			{ test: /\.woff$/,   loader: "file-loader" },
+			{ test: /\.ttf$/,    loader: "file-loader" },
+			{ test: /\.eot$/,    loader: "file-loader" },
+			{ test: /\.svg$/,    loader: "file-loader" }
+		]
+	},
+	output: {
+		path: './dist',
+		filename: 'app.js',
+		publicPath: './dist/'
+	},
+	resolve: {
+		modulesDirectories: ['node_modules'],
+	}
+}
