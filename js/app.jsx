c4837621b5ec423363ec9fef2d998fec54ddbf01
Daniel Büchele <daniel@buechele.cc>
2015-03-07 20:24:08 +0100

    compiled app

--- js/app.jsx
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-require('../less/index.less');
-var UberDoku = require('./UberDoku.jsx');
-
-React.initializeTouchEvents(true);
-React.render(<UberDoku />,document.getElementById("app"));
